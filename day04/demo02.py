3630b57e6d3e3a7efb45da1f10a64c9f92234513
Dowon Lee <edowon0623@gmail.com>
2018-09-20 17:59:21 +0900

    Upload files

--- day04/demo02.py
+++ day04/demo02.py
@@ -1,29 +1,15 @@
-news = """Eying China's voracious demand, 
-Cheniere Energy, ExxonMobil (XOM) and 
-other American energy companies are racing to build 
-more than two dozen expensive facilities to export 
-liquefied natural gas, which is super-cooled natural gas 
-that can be transported by ship.
-"""
+asiaSet = {'korea', 'china', 'japan', 'korea'}
+asiaSet.add('usa')
+asiaSet.add('china')
+asiaSet.remove('japan')
+asiaSet.update({'singapore', 'hongkong', 'korea'})
 
-alphabet = {}  # dict()
-for char in news:
-    if char.isalpha() == False:  # char = .'
-        continue
-    char = char.lower()  # A -> a
-    if char not in alphabet:  # True, False
-        alphabet[char] = 1  # eye -> {'e':1, 'y':1, }
-    else:
-        alphabet[char] += 1  # alphabet[char] = alphabet[char] + 1
+print(asiaSet)
 
-for c in range(ord('a'), ord('z') + 1):
-    print(chr(c), '=', alphabet.get(chr(c), 0))
-
-
-# print(alphabet)
-# 오름차순 정렬 (A -> Z)
-# keyList = list(alphabet.keys())
-# keyList.sort()
-# for key in keyList:
-#     num = alphabet[key]
-#     print(key, '=', num)
+# print(asiaSet)
+# print(type(asiaSet))
+# print(set("dowonlee"))
+# print(set([12, 34, 56, 78, 90]))
+# print(set(('Python', 'Django', 'Html', 'Blockchain')))
+# print(set({'boy':'소년', 'girl':'소녀', 'male':'남성', 'female':'여성'}))
+# print(set())
