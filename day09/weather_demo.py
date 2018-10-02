acea9e33f1437e3403ebaed1801a87ca46143b01
Dowon Lee <edowon0623@gmail.com>
2018-10-02 16:58:50 +0900

    Upload weather demo

--- day09/weather_demo.py
+++ day09/weather_demo.py
@@ -1,27 +1,47 @@
 # weather_demo.py
-# beautifulsoup4
+import pymysql  # import sqlite3
 from bs4 import BeautifulSoup
 from urllib import request
+from weather import Weather
 
-# request.urlopen() 기상청 날씨정보 취득
-# http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108
+# sqllite3.connect('파일명.db')
+conn = pymysql.connect(host='127.0.0.1', user='root', password='', 
+                     db='test_python', charset='utf8') 
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+print("### 1. DB 연결 완료 ###")
+
 target = request.urlopen("http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108")
-
-# BeautifulSoup 클래스는 웹페이지 분석
 soup = BeautifulSoup(target, 'html.parser')
+print("### 2. 날씨정보 취득 완료 ###")
 
 for location in soup.select('location'): # select, select_one 원하는 값 추출
     if location.select_one('city').string == '서울':
         data = location.select('data')
-        print('도시:', location.select_one('city').string)
-
+        #print('도시:', location.select_one('city').string)
+        city = location.select_one('city').string
         for d in data:
-            print('날짜:', d.select_one('tmef').string, end='\t')
-            print('날씨:', d.select_one('wf').string, end='\t')
-            print('최저기온:', d.select_one('tmn').string, end='\t')
-            print('최고기온:', d.select_one('tmx').string, end='\t')
-            print()
+            # print('날짜:', d.select_one('tmef').string, end='\t')
+            # print('날씨:', d.select_one('wf').string, end='\t')
+            # print('최저기온:', d.select_one('tmn').string, end='\t')
+            # print('최고기온:', d.select_one('tmx').string, end='\t')
+            # print()
+
+            # 1) Weather 객체 생성 (13개) -> DB저장
+            weather = Weather(1, city, d.select_one('tmef').string,
+                              d.select_one('wf').string, d.select_one('tmn').string,
+                              d.select_one('tmx').string)
+            sql = """
+               INSERT INTO weather(location, fc_date, description, temp_min, temp_max)
+               VALUES(%s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (weather.location, weather.fc_date, 
+                                weather.description, weather.temp_min, weather.temp_max))
+            
+        conn.commit()
+        print('### 3. {0} 데이터 저장 완료 ###'.format(city))
         break
-    
+
+cursor.close()
+conn.close()    
 
 
