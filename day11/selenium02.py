aee4d8dac00768df8805f4263d6736282d2ebabd
Dowon Lee <edowon0623@gmail.com>
2018-10-08 11:05:58 +0900

    Upload pdf files

--- day11/selenium02.py
+++ day11/selenium02.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from auth import Auth
+from bs4 import BeautifulSoup
 
 auth = Auth('user_info.txt')
 user_id = auth.get('user_id')
@@ -17,4 +18,10 @@ element = driver.find_element_by_name('pass')
 element.send_keys(user_pwd)
 element.send_keys(Keys.RETURN)
 
-# 로그아웃 
+a = driver.find_elements_by_xpath('//*[@id="u_0_a"]/div[1]/div[1]/div/a')
+driver.get(a[0].get_attribute('href'))
+req = driver.page_source
+soup = BeautifulSoup(req, 'html.parser')
+div_list = soup.select('#intro_container_id')
+for info in div_list:
+    print(info.text)
